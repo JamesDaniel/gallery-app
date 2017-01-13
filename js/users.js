b7e9e6f6fe416cf713ea23a4832b8a489b0351a2
James McGarr <james.d.mcgarr@students.ittralee.ie>
2017-01-13 00:53:43 +0000

    added in a currying and closure example added function users.createUser

--- js/users.js
+++ js/users.js
@@ -4,10 +4,10 @@
 var users = (function () {
     var mod = {};
     var users = [
-        {id: 1, name: 'John'},
-        {id: 2, name: 'Pat'},
-        {id: 3, name: 'Peter'},
-        {id: 4, name: 'Paul'}
+        {id: 1, name: 'John', height: 1.6, gender: 'm'},
+        {id: 2, name: 'Patricia', height: 1.7, gender: 'f'},
+        {id: 3, name: 'Peter', height: 1.8, gender: 'm'},
+        {id: 4, name: 'Pauline', height: 1.55, gender: 'f'}
     ];
     mod.getUsers = function () {
         return users;
@@ -21,6 +21,27 @@ var users = (function () {
         return mod.getUsers().find(function (user) {
             return user.name === name;
         });
+    };
+    mod.getNewId = function () {
+        return mod.getUsers().reduce(function (maxId, user) {
+            return user.id > maxId ? user.id + 1 : maxId;
+        }, 1);
+    };
+    // Currying with closure functions.
+    mod.createUser = function (name) {
+        return function (height) {
+            return function (gender) {
+                return {
+                    id: mod.getNewId(),
+                    name: name,
+                    height: height,
+                    gender: gender
+                };
+            };
+        };
+    };
+    mod.addUser = function (user) {
+        mod.getUsers().push(user);
     };
     return mod;
 }());
